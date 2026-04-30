e9fb61664af0d8f27aacd4f73451d7b9a033a0bd
Jose David Baena <josedab@gmail.com>
2026-04-30 20:05:39 +0900

    fix(web): await cleanup before process exit in instrumentation
    Return the cleanup promise and use .finally() to defer process.exit
    until CopilotClient resources are fully released on SIGINT/SIGTERM.

--- apps/web/src/instrumentation.ts
+++ apps/web/src/instrumentation.ts
@@ -6,15 +6,13 @@ export function register() {
 
   // Clean up CopilotClient resources on shutdown
   const cleanup = () => {
-    stopCopilotClient().catch(() => {});
+    return stopCopilotClient().catch(() => {});
   };
   process.on("beforeExit", cleanup);
   process.on("SIGINT", () => {
-    cleanup();
-    process.exit(0);
+    cleanup().finally(() => process.exit(0));
   });
   process.on("SIGTERM", () => {
-    cleanup();
-    process.exit(0);
+    cleanup().finally(() => process.exit(0));
   });
 }
