4d5caf96bbcd0daa3197e4176a23d3544b7d3a4a
Jose David Baena <josedab@gmail.com>
2026-05-09 02:02:57 +0200

    feat(a11y): export LiveRegions and CognitiveLoad from accessibility barrel

--- apps/web/src/components/accessibility/index.ts
+++ apps/web/src/components/accessibility/index.ts
@@ -3,3 +3,5 @@ export type { AccessibilityPreferences } from "./AccessibilityProvider";
 export { AccessibilityPanel } from "./AccessibilityPanel";
 export { SkipLink } from "./SkipLink";
 export { useKeyboardNavigation, useAnnouncer, useReducedMotion, useFocusTrap } from "./hooks";
+export { StreamingAnnouncer, useStreamingAnnouncements, ProgressAnnouncer } from "./LiveRegions";
+export { SimplifiedWizard, ScreenReaderResultsView } from "./CognitiveLoad";
